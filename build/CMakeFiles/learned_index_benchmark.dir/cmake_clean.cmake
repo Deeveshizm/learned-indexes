134d311e29fdb0a3f3c71f5d9613a311aa03c8f0
Deeveshizm <deevesh.jain99@gmail.com>
2025-10-24 13:49:19 -0400

    prototype with build

---
+++ build/CMakeFiles/learned_index_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learned_index_benchmark.dir/learned_index_prototype.cpp.o"
+  "CMakeFiles/learned_index_benchmark.dir/learned_index_prototype.cpp.o.d"
+  "learned_index_benchmark"
+  "learned_index_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learned_index_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
